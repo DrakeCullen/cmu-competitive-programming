6a6538d90e8c2eb2443fd58358e8ae2609536626
Drake C <dpcullen@mavs.coloradomesa.edu>
2022-08-28 12:06:11 -0600

    vector pair example

--- Week-2/vectorTypes.cpp
+++ Week-2/vectorTypes.cpp
@@ -97,6 +97,15 @@ int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cout<<endl<<endl;
 
+    vector<pair<int,int>> vp;
+    vp.push_back(make_pair(1,2));
+    vp.push_back(make_pair(3,4));
+    vp.push_back(make_pair(5,6));
+    for (auto pair : vp) {
+        cout<<pair.first<<" "<<pair.second<<endl;
+    }
+    cout<<endl;
+
     vector<string> names = {"John", "Paul", "George"};
     for(auto elem : names) 
         cout<<elem<<' ';
