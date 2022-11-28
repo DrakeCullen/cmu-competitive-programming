acb0b7e39d91ce538b19da3189e1adcb0ddc976c
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2022-11-28 10:13:29 -0700

    update fenwick tree implementation

--- Week-11/fenwick.cpp
+++ Week-11/fenwick.cpp
@@ -1,84 +1,56 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
+#define LSOne(S) ((S) & -(S))  
 typedef long long ll;
+typedef vector<int> vi;
 typedef vector<ll> vl;
 
-class FenwickTreeRangeQueryPointUpdate {
-	private:
-		// The size of the array holding the Fenwick tree values
-		int N;
+class FenwickTree {                              // index 0 is not used
+private:
+  vl ft;                                        // internal FT is an array
+public:
+  FenwickTree(int m) { ft.assign(m+1, 0); }      // create an empty FT
+  void build(const vl &f) {
+    int m = (int)f.size()-1;                     // note f[0] is always 0
+    ft.assign(m+1, 0);
+    for (int i = 1; i <= m; ++i) {               // O(m)
+      ft[i] += f[i];                             // add this value
+      if (i+LSOne(i) <= m)                       // i has parent
+        ft[i+LSOne(i)] += ft[i];                 // add to that parent
+    }
+  }
 
-		// This array contains the Fenwick tree ranges
-		vl tree;
-		
-		int lsb(int i) {
-			// Isolates the lowest one bit value
-			return i & -i;
-		}
+  FenwickTree(const vl &f) { build(f); }        // create FT based on f
 
-		// Computes the prefix sum from [1, i], O(log(n))
-		long prefixSum(int i) {
-			long sum = 0L;
-			while (i != 0) {
-				sum += tree[i];
-				i &= ~lsb(i); // Equivalently, i -= lsb(i);
-			}
-			return sum;
-		}
-		
-	public:
-		// Create an empty Fenwick Tree with 'sz' parameter zero based.
-		FenwickTreeRangeQueryPointUpdate(int sz) {
-			N = sz + 1;
-			tree.resize(N);
-		}
+  ll rsq(int j) {                                // returns RSQ(1, j)
+    ll sum = 0;
+    for (; j; j -= LSOne(j))
+      sum += ft[j];
+    return sum;
+  }
 
-		// Construct a Fenwick tree with an initial set of values.
-		// The 'values' array MUST BE ONE BASED meaning values[0]
-		// does not get used, O(n) construction.
-		FenwickTreeRangeQueryPointUpdate(vl values) {
-			N = values.size();
-			values[0] = 0L;
+  ll rsq(int i, int j) { return rsq(j) - rsq(i-1); } // inc/exclusion
 
-			// Make a clone of the values array since we manipulate
-			// the array in place destroying all its original content.
-			tree = values;
-
-			for (int i = 1; i < N; i++) {
-				int parent = i + lsb(i);
-				if (parent < N) tree[parent] += tree[i];
-			}
-		}		
-
-		// Returns the sum of the interval [left, right], O(log(n))
-		long sum(int left, int right) {
-			return prefixSum(right) - prefixSum(left - 1);
-		}
-
-		// Get the value at index i
-		long get(int i) {
-			return sum(i, i);
-		}
-
-		// Add 'v' to index 'i', O(log(n))
-		void add(int i, long v) {
-			while (i < N) {
-				tree[i] += v;
-				i += lsb(i);
-			}
-		}
-
-		// Set index i to be equal to v, O(log(n))
-		void set(int i, long v) {
-			add(i, v - sum(i, i));
-		}
+  // updates value of the i-th element by v (v can be +ve/inc or -ve/dec)
+  void update(int i, ll v) {
+    for (; i < (int)ft.size(); i += LSOne(i))
+      ft[i] += v;
+  }
 };
 
 int main() {
 	vl input = {0, 3, 4, -2, 7, 3, 11, 5, -8, -9, 2, 4, -8};
-	FenwickTreeRangeQueryPointUpdate ft(input);
-	cout<<ft.sum(1,3);
-	return 0;
+	FenwickTree ft(input);
+	cout << "Original vector: ";
+	for (unsigned i = 1; i < input.size(); i++) cout << input[i] << " ";
+	cout << endl;
+
+	cout<< "rsq(1,3) = " << ft.rsq(1,3)<<endl;
+	cout<< "rsq(1,4) = " << ft.rsq(1,4)<<endl;
+	cout<<endl;
+	
+	ft.update(3, 5);
+	cout<< "rsq(1,3) = " <<ft.rsq(1,3)<<endl;
+	cout<< "rsq(1,4) = " << ft.rsq(1,4)<<endl;
 }
